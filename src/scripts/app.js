2316d2c6132165e47daf8392a1cc5225426387d7
Jeff S <jstein0@searshc.com>
2017-03-21 14:32:01 -0500

    create type filter.  start product filter.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,10 +9,22 @@ $(function() {
     collections:{},
     routers:{},
     init: function() {
-      matrix = new app.views.Suppliers(suppliers);
+      myMatrix = new app.views.Suppliers(suppliers);
+      myRouter = new app.routers.Router();
+      Backbone.history.start();
     }
 
   }
+
+  app.routers.Router = Backbone.Router.extend({
+    routes:{
+      'filter/:type':'urlFilter'
+    },
+    urlFilter: function(type) {
+      myMatrix.filterType = type;
+      myMatrix.trigger('change:filterType');
+    }
+  });
 
   app = app || {};
   app.models.Vendor = Backbone.Model.extend({
@@ -41,14 +53,14 @@ $(function() {
   });
 
   app.views.Vendor = Backbone.View.extend({
-    tagname:'li',
+    tagName:'li',
     attributes: function() {
       return {
-        class: 'vendor' + this.model.get('type')
+        class: 'vendor ' + this.model.get('type')
       };
     },
     events: {
-      'click .vendor-header': 'showDetails'
+      'click .header': 'showDetails'
     },
     template: _.template($('#vendor-template').html()),
     render: function() {
@@ -66,11 +78,16 @@ $(function() {
       initialize: function(data) {
         this.collection = new app.collections.Suppliers(data);
         this.render();
+        this.$el.find('#filtering').append(this.createTypeFilter());
+        this.$el.find('#filtering').append(this.createProductFilter());
         this.on('change:searchFilter', this.filterBySearch, this);
+        this.on('change:filterType', this.filterByType, this);
+        this.on('change:filterProduct', this.filterByProduct, this);
         this.collection.on('reset', this.render, this);
       },
       events: {
-        'keyup #searchBox':'searchFilter'
+        'keyup #searchBox':'searchFilter',
+        'click a.filter':'setFilter'
       },
       render: function() {
         var self = this;
@@ -85,25 +102,84 @@ $(function() {
         });
         $('#suppliers').append(newVendor.render().el);
       },
+      getTypes: function() {
+        return _.uniq(this.collection.pluck('type'))
+      },
+      createTypeFilter: function() {
+        var filters = '<h5>type filter</h5><a class="btn btn-default filter" role="button" href="#all">all</a>';
+        _.each(this.getTypes(), function(item) {
+          filters += '<a class="btn btn-default filter" role="button" href="#'+item+'">'+item+'</a>';
+        })
+        return filters;
+      },
+      getProducts: function() {
+        return _.uniq(this.collection.pluck('products'))
+      },
+      createProductFilter: function() {
+        console.log('this.getProducts(): ', this.getProducts());
+        var allItems = [];
+        var filters = '<h5>product filter</h5><a class="btn btn-default filter" role="button" href="#all">all</a>';
+        _.each(this.getProducts(), function(items) {
+          console.log('items: ', items);
+          _.each(items, function(item) {
+            allItems.push(item);
+          })
+        })
+        console.log('allItems: ', allItems);
+        var uniqueItems = _.uniq(allItems);
+        console.log('uniqueItems: ', uniqueItems);
+        _.each(uniqueItems, function(item) {
+          filters += '<a class="btn btn-default filter" role="button" href="#'+item+'">'+item+'</a>';
+        })
+        return filters;
+      },
+      getStyles: function() {
+        return _.uniq(this.collection.pluck('styles'))
+      },
+      getReview: function() {
+        return _.uniq(this.collection.pluck('review'))
+      },
       searchFilter: function(e) {
         this.searchFilter = e.target.value;
         this.trigger('change:searchFilter');
       },
       filterBySearch: function() {
-        console.log('this: ', this);
+        console.log('START this.collection.length: ', this.collection.length);
         this.collection.reset(suppliers, {silent:true});
         var filterString = this.searchFilter,
           filtered = _.filter(this.collection.models, function(item) {
-            console.log('item: ', item.attributes.products);
+            // console.log('item: ', item.attributes.products);
             // check "name" node (string) of item for index of searched string.
-            return item.get('name').toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
+            // return item.get('name').toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
             // console.log('item.products: ', item.products);
             // how do I check an array for the string value?
             // this method only matches full search term:
-            // return item.get('products').indexOf(filterString.toLowerCase()) !== -1;
+            return item.get('products').indexOf(filterString.toLowerCase()) !== -1;
             // probably need to iterate over products array to get better result.
+            // return $.inArray(filterString.toLowerCase(), item.get('products'));
           });
         this.collection.reset(filtered);
+        console.log('END this.collection.length: ', this.collection.length);
+      },
+      setFilter: function(e) {
+        e.preventDefault();
+        this.filterType = e.currentTarget.innerHTML;
+        this.trigger('change:filterType');
+      },
+      filterByType: function() {
+        if (this.filterType === 'all') {
+          this.collection.reset(suppliers);
+          myRouter.navigate('filter/all');
+        } else {
+          this.collection.reset(suppliers, {silent:true});
+          var filterType = this.filterType,
+            filtered = _.filter(this.collection.models, function(item) {
+              return item.get('type') === filterType;
+            });
+          this.collection.reset(filtered);
+          myRouter.navigate('filter/'+filterType);
+        }
+        console.log('this.collection: ', this.collection.length);
       }
   });
 
