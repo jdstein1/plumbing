4373702d528753feb3128b968efd1f9069126ae6
Jeff S <jstein0@searshc.com>
2017-03-21 14:49:04 -0500

    success with product filter.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,16 +13,20 @@ $(function() {
       myRouter = new app.routers.Router();
       Backbone.history.start();
     }
-
   }
 
   app.routers.Router = Backbone.Router.extend({
     routes:{
-      'filter/:type':'urlFilter'
+      'filter/:type':'urlFilterType',
+      'filter/:product':'urlFilterProduct'
     },
-    urlFilter: function(type) {
+    urlFilterType: function(type) {
       myMatrix.filterType = type;
       myMatrix.trigger('change:filterType');
+    },
+    urlFilterProduct: function(product) {
+      myMatrix.filterProduct = product;
+      myMatrix.trigger('change:filterProduct');
     }
   });
 
@@ -78,16 +82,17 @@ $(function() {
       initialize: function(data) {
         this.collection = new app.collections.Suppliers(data);
         this.render();
-        this.$el.find('#filtering').append(this.createTypeFilter());
+        // this.$el.find('#filtering').append(this.createTypeFilter());
         this.$el.find('#filtering').append(this.createProductFilter());
         this.on('change:searchFilter', this.filterBySearch, this);
-        this.on('change:filterType', this.filterByType, this);
+        // this.on('change:filterType', this.filterByType, this);
         this.on('change:filterProduct', this.filterByProduct, this);
         this.collection.on('reset', this.render, this);
       },
       events: {
         'keyup #searchBox':'searchFilter',
-        'click a.filter':'setFilter'
+        'click a.filter.type':'setFilterType',
+        'click a.filter.product':'setFilterProduct'
       },
       render: function() {
         var self = this;
@@ -106,9 +111,9 @@ $(function() {
         return _.uniq(this.collection.pluck('type'))
       },
       createTypeFilter: function() {
-        var filters = '<h5>type filter</h5><a class="btn btn-default filter" role="button" href="#all">all</a>';
+        var filters = '<h5>filter by type</h5><a class="btn btn-default filter type" role="button" href="#all">all</a>';
         _.each(this.getTypes(), function(item) {
-          filters += '<a class="btn btn-default filter" role="button" href="#'+item+'">'+item+'</a>';
+          filters += '<a class="btn btn-default filter type" role="button" href="#'+item+'">'+item+'</a>';
         })
         return filters;
       },
@@ -116,20 +121,20 @@ $(function() {
         return _.uniq(this.collection.pluck('products'))
       },
       createProductFilter: function() {
-        console.log('this.getProducts(): ', this.getProducts());
+        // console.log('this.getProducts(): ', this.getProducts());
         var allItems = [];
-        var filters = '<h5>product filter</h5><a class="btn btn-default filter" role="button" href="#all">all</a>';
+        var filters = '<h5>filter by product</h5><a class="btn btn-default filter product" role="button" href="#all">all</a>';
         _.each(this.getProducts(), function(items) {
-          console.log('items: ', items);
+          // console.log('items: ', items);
           _.each(items, function(item) {
             allItems.push(item);
           })
         })
-        console.log('allItems: ', allItems);
+        // console.log('allItems: ', allItems);
         var uniqueItems = _.uniq(allItems);
-        console.log('uniqueItems: ', uniqueItems);
+        // console.log('uniqueItems: ', uniqueItems);
         _.each(uniqueItems, function(item) {
-          filters += '<a class="btn btn-default filter" role="button" href="#'+item+'">'+item+'</a>';
+          filters += '<a class="btn btn-default filter product" role="button" href="#'+item+'">'+item+'</a>';
         })
         return filters;
       },
@@ -150,23 +155,24 @@ $(function() {
           filtered = _.filter(this.collection.models, function(item) {
             // console.log('item: ', item.attributes.products);
             // check "name" node (string) of item for index of searched string.
-            // return item.get('name').toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
+            return item.get('name').toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
             // console.log('item.products: ', item.products);
             // how do I check an array for the string value?
             // this method only matches full search term:
-            return item.get('products').indexOf(filterString.toLowerCase()) !== -1;
+            // return item.get('products').indexOf(filterString.toLowerCase()) !== -1;
             // probably need to iterate over products array to get better result.
             // return $.inArray(filterString.toLowerCase(), item.get('products'));
           });
         this.collection.reset(filtered);
         console.log('END this.collection.length: ', this.collection.length);
       },
-      setFilter: function(e) {
+      setFilterType: function(e) {
         e.preventDefault();
         this.filterType = e.currentTarget.innerHTML;
         this.trigger('change:filterType');
       },
       filterByType: function() {
+        console.log('this.filterType: ', this.filterType);
         if (this.filterType === 'all') {
           this.collection.reset(suppliers);
           myRouter.navigate('filter/all');
@@ -178,6 +184,27 @@ $(function() {
             });
           this.collection.reset(filtered);
           myRouter.navigate('filter/'+filterType);
+        }
+        console.log('this.collection: ', this.collection.length);
+      },
+      setFilterProduct: function(e) {
+        e.preventDefault();
+        this.filterProduct = e.currentTarget.innerHTML;
+        this.trigger('change:filterProduct');
+      },
+      filterByProduct: function() {
+        console.log('this.filterProduct: ', this.filterProduct);
+        if (this.filterProduct === 'all') {
+          this.collection.reset(suppliers);
+          myRouter.navigate('filter/all');
+        } else {
+          this.collection.reset(suppliers, {silent:true});
+          var filterProduct = this.filterProduct,
+            filtered = _.filter(this.collection.models, function(item) {
+              return item.get('products').includes(filterProduct);
+            });
+          this.collection.reset(filtered);
+          myRouter.navigate('filter/'+filterProduct);
         }
         console.log('this.collection: ', this.collection.length);
       }
