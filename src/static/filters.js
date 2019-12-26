2d3c411c53a8bb634611e547435f660c9057eda7
Jeff S <jdstein1@gmail.com>
2019-12-26 14:41:01 -0600

    updated file structure and styling...
    also put data in own static files.

---
+++ src/static/filters.js
@@ -0,0 +1,16 @@
+const filters = [
+    "prod_bidets",
+    "prod_cabinets",
+    "prod_countertops",
+    "prod_faucets",
+    "prod_hardware",
+    "prod_lighting",
+    "prod_shower_doors",
+    "prod_sinks",
+    "prod_toilets",
+    "prod_towel_warmers",
+    "prod_tubs_acrylic",
+    "prod_tubs_castiron",
+];
+
+export default filters;
